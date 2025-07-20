31e1247a2cf87a7792a94b9e8693ff675ce3be3c
ChoiMari <chl98639863@gmail.com>
2025-07-21 00:35:48 +0900

    feat: Role Entity구현(app_role 테이블과 매핑됨)

--- bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/domain/Role.java
+++ bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/domain/Role.java
@@ -1,4 +1,20 @@
 package com.yakgurt.bokyakjigi.user.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor // 기본 생성자 자동 생성
+@AllArgsConstructor(access = AccessLevel.PRIVATE) // 모든 필드 값 파라미터로 받는 생성자 생성, 접근 제한자는 private
+@Builder // 빌더 패턴 사용
+@Getter
+@ToString(callSuper = true) // toString() 자동 생성, 부모 클래스 필드도 포함
+@Entity // JPA 엔티티임을 명시, DB 테이블과 매핑됨
+@Table(name = "APP_ROLE") // 엔티티가 매핑될 테이블 이름 지정
 public class Role {
+    @Id // PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "ROLE_NAME", nullable = false, length = 20)
+    private String roleName;
 }
