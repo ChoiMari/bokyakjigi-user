1cdc389478198ce39c9188d292840d05ca402448
ChoiMari <chl98639863@gmail.com>
2025-07-21 00:37:01 +0900

    feat: ProfileImg Entity 구현(profile_img 테이블과 매핑됨)

--- bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/domain/ProfileImg.java
+++ bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/domain/ProfileImg.java
@@ -1,4 +1,32 @@
 package com.yakgurt.bokyakjigi.user.domain;
 
-public class ProfileImg {
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+@ToString(exclude = "member")
+@Entity
+@Table(name = "PROFILE_IMG")
+public class ProfileImg { // 프로필 이미지 정보를 저장하는 엔티티
+    @Id //PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY) // Member와 1:1
+    @JoinColumn(name = "MEMBER_ID", nullable = false, unique = true)
+    @Basic(optional = false)
+    private Member member;
+
+    @Column(name = "IMG_URL", nullable = false, length = 3000)
+    @Basic(optional = false)
+    private String imgUrl;
+
+    @Column(name = "UPLOADED_AT", nullable = false)
+    @Basic(optional = false)
+    private LocalDateTime uploadedAt;
 }
