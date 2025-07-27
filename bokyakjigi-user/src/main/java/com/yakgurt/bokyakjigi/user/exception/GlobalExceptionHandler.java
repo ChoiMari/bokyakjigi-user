1e7a49e325a11c0d81d6d7b5119fe0c3dbce6080
ChoiMari <chl98639863@gmail.com>
2025-07-27 15:51:35 +0900

    refactor : ResponseEntity<String> -> ResponseEntity<ErrorReesponse>로 변경하여 통일된 형식의 json으로 응답 보낼 수 있도록 변경함

--- bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/exception/GlobalExceptionHandler.java
+++ bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/exception/GlobalExceptionHandler.java
@@ -6,8 +6,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 /**
  * 전역적으로 예외를 처리해주는 클래스
+ * TODO : 추후 enum으로 상태 코드와 메세지 관리하는 로직으로 변경하기, 다국어 처리 고려
  */
 @Slf4j
 @RestControllerAdvice // 전역 예외 처리용 어노테이션(@ControllerAdvice + @ResponseBody 역할), 모든 컨트롤러에서 발생하는 예외를 잡아 JSON형태로 응답을 만들어 준다(그래야 프런트에서 파싱이 가능함)
@@ -22,13 +25,22 @@ public class GlobalExceptionHandler {
      * @return 클라이언트에게 HTTP 상태코드 401과 예외 메시지를 함께 전달
      */
     @ExceptionHandler(TokenExpiredException.class)
-    public ResponseEntity<String> handleTokenExpired(TokenExpiredException ex){
+    public ResponseEntity<ErrorResponse> handleTokenExpired(TokenExpiredException ex){ //ResponseEntity<String> -> 에러 응답객체 DTO로 변경함
         log.warn("[JWT] 만료된 토큰으로 인한 예외 발생: {}", ex.getMessage(), ex);
 
-        //클라이언트에게 HTTP 응답 보냄
-        return  ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED) // 상태코드: 401 UNAUTHORIZED (인증 실패)
-                .body(ex.getMessage());          // 응답 본문(body)에 메시지 담기
+
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.UNAUTHORIZED.value(), // 상태코드
+                "UNAUTHORIZED", // 상태 타입 // TODO : 추후 enum으로 뽑으면 깔끔함
+                ex.getMessage(), // 예외 메세지
+                LocalDateTime.now() // 예외 발생 시각
+        );
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+//        //클라이언트에게 HTTP 응답 보냄
+//        return  ResponseEntity
+//                .status(HttpStatus.UNAUTHORIZED) // 상태코드: 401 UNAUTHORIZED (인증 실패)
+//                .body(ex.getMessage());          // 응답 본문(body)에 메시지 담기
     }
 
     /**
@@ -38,12 +50,18 @@ public class GlobalExceptionHandler {
      * @return HTTP 응답 객체(상태 코드 401 + 예외메세지)
      */
     @ExceptionHandler(InvalidTokenException.class)
-    public ResponseEntity<String> handleInvalidToken(InvalidTokenException ex){
+    public ResponseEntity<ErrorResponse> handleInvalidToken(InvalidTokenException ex){
         log.warn("[JWT] 유효하지 않은 토큰 예외 발생 : {}", ex.getMessage(),ex);
-
-        return ResponseEntity
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.UNAUTHORIZED.value(), // 상태 코드
+                "UNAUTHORIZED", // 상태 타입
+                ex.getMessage(), // 예외 메세지
+                LocalDateTime.now() // 예외 발생 시각
+        );
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+       /* return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED) // 상태 코드 : 401
-                .body(ex.getMessage()); //응답 본문(body)에 메세지 담음
+                .body(ex.getMessage()); //응답 본문(body)에 메세지 담음  */
     }
 
     /**
@@ -54,11 +72,19 @@ public class GlobalExceptionHandler {
      * @return HTTP 응답 객체(상태 코드 400 + 예외 메세지)
      */
     @ExceptionHandler(MissingUserClaimException.class)
-    public ResponseEntity<String> handleMissingUserClaim(MissingUserClaimException ex){
+    public ResponseEntity<ErrorResponse> handleMissingUserClaim(MissingUserClaimException ex){
         log.warn("[JWT] 사용자 정보 누락 예외 발생 : {}", ex.getMessage(), ex);
-        return ResponseEntity
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "BAD_REQUEST",
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+
+       /* return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST) // 상태 코드 400
-                .body(ex.getMessage());
+                .body(ex.getMessage()); */
     }
     //<---
 
@@ -72,11 +98,20 @@ public class GlobalExceptionHandler {
      * @return HTTP 응답 객체(상태 코드 500 + 예외 메세지)
      */
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleAllException(Exception ex) {
+    public ResponseEntity<ErrorResponse> handleAllException(Exception ex) {
         log.error("예상치 못한 예외 발생: {}", ex.getMessage(), ex);
-        return ResponseEntity
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "INTERNAL_SERVER_ERROR",
+                "서버 내부 오류가 발생했습니다. 관리자에게 문의하세요.",
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        /* return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR) // 상태 코드 500
                 .body("서버 내부 오류가 발생했습니다. 관리자에게 문의하세요."); // 보안 때문에 .body(ex.getMessage());를 사용하지 않음
         // 내부 시스템 정보 노출 방지
+         */
     }
 }
