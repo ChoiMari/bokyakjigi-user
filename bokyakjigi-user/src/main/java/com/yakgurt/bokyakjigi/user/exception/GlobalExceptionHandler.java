80d2305aded173da2ed9550b2e1a0a44d99f5a37
ChoiMari <chl98639863@gmail.com>
2025-07-27 15:53:59 +0900

    docs : todo 주석보완

--- bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/exception/GlobalExceptionHandler.java
+++ bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/exception/GlobalExceptionHandler.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 
 /**
  * 전역적으로 예외를 처리해주는 클래스
- * TODO : 추후 enum으로 상태 코드와 메세지 관리하는 로직으로 변경하기, 다국어 처리 고려
+ * TODO : 추후 enum으로 상태 코드와 메세지 관리하는 로직으로 변경하기, 다국어 처리 고려(message에 메시지 코드 넣고 MessageSource로 메시지 resolve)
  */
 @Slf4j
 @RestControllerAdvice // 전역 예외 처리용 어노테이션(@ControllerAdvice + @ResponseBody 역할), 모든 컨트롤러에서 발생하는 예외를 잡아 JSON형태로 응답을 만들어 준다(그래야 프런트에서 파싱이 가능함)
