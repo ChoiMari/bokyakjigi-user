88b3cdc301bab4abdbf966e73a3ff2b1c9022710
ChoiMari <chl98639863@gmail.com>
2025-07-27 18:18:30 +0900

    chore : config -> security 폴더로 파일위치 변경

--- bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/security/SecurityConfig.java
+++ bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.yakgurt.bokyakjigi.user.config;
+package com.yakgurt.bokyakjigi.user.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
