2337e7d53d1d17c62dacca566e2f880b40eeae29
ChoiMari <chl98639863@gmail.com>
2025-08-02 10:59:49 +0900

    refactor : LocalDateTime에서 ZoneDateTime으로 타입변경함(서버시간대포함시킴)

--- bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/exception/ErrorResponse.java
+++ bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/exception/ErrorResponse.java
@@ -1,9 +1,11 @@
 package com.yakgurt.bokyakjigi.user.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 /**
  * 에러 응답 객체 DTO : 프런트에 통일된 형식의 json으로 보내기 위해 응답 DTO를 정의
@@ -15,7 +17,10 @@ public class ErrorResponse {
     private int code;         // HTTP 상태 코드
     private String error;     // 오류 타입 (예: Unauthorized)
     private String message;   // 상세 오류 메시지
-    private LocalDateTime timestamp; // 예외 발생 시각
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssXXX", timezone = "UTC")
+    private ZonedDateTime timestamp;
+    //private LocalDateTime timestamp; // 예외 발생 시각
 
 }
 
