81df6940a70ded26a7199375f378375d0775c309
ChoiMari <chl98639863@gmail.com>
2025-08-06 01:30:57 +0900

    feat: 회원가입 메서드 추가

--- bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/service/SignUpServiceImpl.java
+++ bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/service/SignUpServiceImpl.java
@@ -1,6 +1,10 @@
 package com.yakgurt.bokyakjigi.user.service;
 
+import com.yakgurt.bokyakjigi.user.domain.Member;
+import com.yakgurt.bokyakjigi.user.domain.MemberRole;
+import com.yakgurt.bokyakjigi.user.domain.ProfileImg;
 import com.yakgurt.bokyakjigi.user.dto.SignUpRequestDto;
+import com.yakgurt.bokyakjigi.user.exception.RoleNotFoundException;
 import com.yakgurt.bokyakjigi.user.repository.MemberRepository;
 import com.yakgurt.bokyakjigi.user.repository.ProfileImgRepository;
 import com.yakgurt.bokyakjigi.user.repository.RoleRepository;
@@ -11,6 +15,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.yakgurt.bokyakjigi.user.exception.DuplicateNicknameException;
 import com.yakgurt.bokyakjigi.user.exception.DuplicateEmailException;
+import com.yakgurt.bokyakjigi.user.domain.Role;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -21,27 +28,49 @@ public class SignUpServiceImpl implements SignUpService {
     private final RoleRepository roleRepo;
     private final ProfileImgRepository profileImgRepo;
     private final PasswordEncoder pwdEncoder;
-
     private static final String DEFAULT_PROFILE_URL = "/images/default-profile.png";
 
+    /**
+     * 회원가입 처리 메서드
+     * @param dto 회원가입 요청 DTO
+     * @return 가입 완료된 회원의 id(고유 식별자, PK)
+     */
     @Override
     @Transactional // 모두 성공해야 commit처리, 하나의 트랜잭션으로 묶음
     public Long signUp(SignUpRequestDto dto) {
-        // 이메일 중복검사(중복이면 예외)
+        log.debug("signUp(email={},nickname={})",dto.getEmail(),dto.getNickname()); //-> 비밀번호 절대 찍으면 안됨
+        //-> 이메일은 회사마다 정책에 따라서 로그 찍는 여부 다르다고 함
 
-        // 닉네임 중복 검사(중복이면 예외)
+        // 이메일 중복검사(중복이면 DuplicateEmailException 예외)
+        checkDuplicateEmail(dto.getEmail());
+
+        // 닉네임 중복 검사(중복이면 DuplicateNicknameException 예외)
+        checkDuplicateNickname(dto.getNickname());
 
         // ROLE_USER 조회(없으면 예외 발생)
+        Role roleUser = roleRepo.findByRoleName(MemberRole.USER)
+                .orElseThrow(() -> new RoleNotFoundException("USER 권한이 없습니다.")); // 전역예외처리기에서 처리
 
         // 비밀번호 암호화
+        String encodedPassword = pwdEncoder.encode(dto.getPassword());
 
         // Member 엔터티 생성
+        Member member = dto.toEntity(encodedPassword, roleUser);
 
         // 저장(DB에 insert)
+        Member savedMember = memberRepo.save(member);
 
-        // 기본 프로필 이미지 엔터티 생성 및 저장(DB에 insert)
+        // 가입한 회원정보로 프로필 이미지 생성
+        ProfileImg profileImg = ProfileImg.builder()
+                .member(savedMember)  // member 객체를 넣으면 JPA가 FK인 MEMBER_ID 컬럼에 member.getId() 저장
+                .imgUrl(DEFAULT_PROFILE_URL)
+                .uploadedAt(LocalDateTime.now())
+                .build();
 
-        return 0L;
+        //저장(DB에 insert)
+        profileImgRepo.save(profileImg);
+
+        return savedMember.getId(); // 가입한 회원 PK 반환
     }
 
     /**
