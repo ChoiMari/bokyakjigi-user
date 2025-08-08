bf10b27d8f9146d4e9bf8c86d8b023093967d32c
ChoiMari <chl98639863@gmail.com>
2025-08-08 12:32:42 +0900

    feat : memberId 뽑아내는 메서드 추가

--- bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/repository/ProfileImgRepository.java
+++ bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/repository/ProfileImgRepository.java
@@ -3,6 +3,9 @@ package com.yakgurt.bokyakjigi.user.repository;
 import com.yakgurt.bokyakjigi.user.domain.ProfileImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfileImgRepository extends JpaRepository<ProfileImg, Long> {
+    Optional<ProfileImg> findByMember_Id(Long memberId);
 
 }
