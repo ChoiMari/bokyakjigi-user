0708364313285e735367c9c25df89109a89a37cf
ChoiMari <chl98639863@gmail.com>
2025-08-15 19:29:22 +0900

    fix : refesh토큰도 같이 반환하기 위해서 dto타입으로 리턴타입 변경, findByEmailAndIsDelete호출 시 아규먼트 enum값으로 넣음

--- bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/service/AuthService.java
+++ bokyakjigi-user/src/main/java/com/yakgurt/bokyakjigi/user/service/AuthService.java
@@ -1,7 +1,9 @@
 package com.yakgurt.bokyakjigi.user.service;
 
 import com.yakgurt.bokyakjigi.user.config.jwt.JwtProperties;
+import com.yakgurt.bokyakjigi.user.domain.IsDeleted;
 import com.yakgurt.bokyakjigi.user.domain.Member;
+import com.yakgurt.bokyakjigi.user.dto.SignInResponseDto;
 import com.yakgurt.bokyakjigi.user.repository.MemberRepository;
 import com.yakgurt.bokyakjigi.user.security.JwtProvider;
 import com.yakgurt.bokyakjigi.user.vo.MemberVO;
@@ -34,16 +36,16 @@ public class AuthService {
      * 로그인 처리 메서드
      * @param email 사용자 이메일 (로그인 ID)
      * @param rawPassword 입력된 비밀번호 (평문)
-     * @return 유효한 JWT 액세스 토큰 문자열
+     * @return SignInResponseDto 반환 (accessToken, refreshToken, tokenType 포함)
      * @throws UsernameNotFoundException 회원이 존재하지 않을 경우
      * @throws BadCredentialsException 비밀번호 불일치 시
      * 전역 예외 처리 클래스에서 json형태로 보내야함 -> 작업완료
      */
     @Transactional(readOnly = true)  // 읽기 전용 트랜잭션 (성능 최적화용)
-    public String signIn(String email, String rawPassword) {
+    public SignInResponseDto signIn(String email, String rawPassword) {
         log.info("signIn start (email={})",email);
         // 이메일과 탈퇴 여부 'N'로 활성 회원 조회
-        Member member = memberRepository.findByEmailAndIsDeleted(email, "N") //email이 일치하고, isDeleted가 N인 MEMBER객체를 찾음
+        Member member = memberRepository.findByEmailAndIsDeleted(email, IsDeleted.N) //email이 일치하고, isDeleted가 N인 MEMBER객체를 찾음
                 .orElseThrow(() -> new UsernameNotFoundException("가입된 회원이 없습니다."));
         //해당 값이 없을 경우 UsernameNotFoundException예외 발생
 
@@ -55,7 +57,7 @@ public class AuthService {
         MemberVO memberVO = new MemberVO(member.getId(), member.getEmail(), member.getNickname(), member.getRole().getRoleName()); // 필요한 필드만
         // 엑세스 토큰
         Duration accessTokenValidity = Duration.ofMinutes(jwtProperties.getAccessTokenExpirationMinutes()); // 토큰 만료시간
-        String token = jwtProvider.generateAccessToken(accessTokenValidity, memberVO);
+        String accessToken = jwtProvider.generateAccessToken(accessTokenValidity, memberVO);
 
         //Refresh Token 유효기간
         Duration refreshTokenValidity = Duration.ofDays(jwtProperties.getRefreshTokenExpirationDays());
@@ -67,7 +69,7 @@ public class AuthService {
         String redisKey = "RT:" + member.getId(); // RT : RefreshToken 약어
         redisTemplate.opsForValue().set(redisKey, refreshToken, refreshTokenValidity);
 
-        log.info("signIn success - email={}", email);
-        return token;
+        log.info("signIn success - email={} Redis Key={}", email, redisKey);
+        return new SignInResponseDto(accessToken, refreshToken);
     }
 }
